1af7a66cd0e6b16ee12831ae2b5e6e47b02c5e42
Abiy Hailu <abiyhailu120@gmail.com>
2026-06-26 17:03:27 +0300

    fix:remove-unused-imports-from-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
 import Quiz from "@/components/quiz";
-import { useState } from "react";
-import { Card, CardContent } from "./components/ui/card";
-import { useAtom } from "jotai";
 
 const App = () => {
-  const [finished, setFinished] = useState<boolean>(false);
-
-  const [correctAnswered, setCorrectAnswered] = useState<number>(0);
-  // const handleClick = (correct: boolean) => {
-  //   if (currentQuizIndex < quizs.length - 1) {
-  //     setCurrentQuizIndex(currentQuizIndex + 1);
-  //     if (correct) {
-  //       setCorrectAnswered(correctAnswered + 1);
-  //     }
-  //   } else {
-  //     setFinished(true);
-  //   }
-  // };
   return (
     <div className="flex flex-col space-y-4 justify-center items-center min-h-lvh">
       <p className="text-4xl">Quiz App</p>
