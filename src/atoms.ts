a656d5f44d14e43f99e07bda7e4c3f3e4f615a9e
Abiy Hailu <abiyhailu120@gmail.com>
2026-06-26 16:41:10 +0300

    feat:finish-quiz

--- src/atoms.ts
+++ src/atoms.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 
 export const currentQuizAtom = atom<number>(0);
 export const quizOverAtom = atom<boolean>(false);
+export const answeredQuizsAtom = atom<number>(0);
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 import { useState } from "react";
 import { CircleCheck, CircleX } from "lucide-react";
 import { useAtom } from "jotai";
-import { currentQuizAtom, quizOverAtom } from "@/atoms";
+import { answeredQuizsAtom, currentQuizAtom, quizOverAtom } from "@/atoms";
 import { quizs } from "@/data";
 
 const Quiz = () => {
@@ -14,8 +14,14 @@ const Quiz = () => {
   const [quizOver, setQuizOver] = useAtom(quizOverAtom);
   const quiz = quizs[currentQuizIndex];
   const [selectedValue, setSelectedValue] = useState<string>("");
-  // const [correctAnswered, setCorrectAnswered] = useState<boolean | null>(null);
+  const [answered, setAnswered] = useState(false);
+  const [correctAnswered, setCorrectAnswered] = useState<boolean | null>(null);
+  const [answeredQuizs, setAnsweredQuizs] = useAtom(answeredQuizsAtom);
   const handleClick = () => {
+    setAnswered(true);
+    setTimeout(() => {
+      setAnswered(false);
+    }, 1000);
     if (currentQuizIndex < quizs.length - 1) {
       setCurrentQuizIndex(currentQuizIndex + 1);
     }
@@ -23,60 +29,92 @@ const Quiz = () => {
       setQuizOver(true);
     }
     if (quiz.correctAnswerIndex == parseInt(selectedValue)) {
+      setCorrectAnswered(true);
+      setAnsweredQuizs(answeredQuizs + 1);
+    } else {
+      setCorrectAnswered(false);
     }
   };
   return (
     <>
-      <Card className="w-full max-w-sm">
-        {/* {correctAnswered !== null && (
-          <>
-            <CardContent className="w-full flex flex-col items-center space-y-3">
-              {correctAnswered ? (
-                <CircleCheck color="green" size={60} />
-              ) : (
-                <>
-                  <CircleX color="red" size={60} />
-                  <FieldLabel>
-                    <Field orientation="horizontal">
-                      <FieldContent>
-                        <span className="text-green-800">
-                          Correct Answer:{" "}
-                          {quiz.choices[quiz.correctAnswerIndex]}
-                        </span>
-                      </FieldContent>
-                    </Field>
-                  </FieldLabel>
-                </>
-              )}
-            </CardContent>
-          </>
-        )}
-          <> */}
-        <CardHeader>
-          <CardTitle>
-            {currentQuizIndex + 1}. {quiz.question}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <RadioGroup onValueChange={setSelectedValue} value={selectedValue}>
-            {quiz.choices.map((choice, index) => (
-              <FieldLabel key={index} className="bg-green-300/50">
-                <Field orientation="horizontal">
-                  <FieldContent>{choice}</FieldContent>
-                  <RadioGroupItem value={index.toString()} />
-                </Field>
-              </FieldLabel>
-            ))}
-          </RadioGroup>
-        </CardContent>
-      </Card>
-      <Button
-        className="w-full max-w-sm"
-        onClick={handleClick}
-        disabled={currentQuizIndex > quizs.length - 1}
-      >
-        Choose
-      </Button>
+      {quizOver && (
+        <Card className="w-full max-w-sm">
+          <CardContent className="flex flex-col items-center">
+            <p>Your have finished the quiz</p>
+          </CardContent>
+        </Card>
+      )}
+      {!quizOver && (
+        <>
+          <Card className="w-full max-w-sm">
+            <CardHeader>
+              {answeredQuizs} / {quizs.length}
+            </CardHeader>
+            {answered && (
+              <>
+                <CardContent className="w-full flex flex-col items-center space-y-3">
+                  {correctAnswered ? (
+                    <CircleCheck color="green" size={60} />
+                  ) : (
+                    <>
+                      <CircleX color="red" size={60} />
+                      <FieldLabel>
+                        <Field orientation="horizontal">
+                          <FieldContent>
+                            <span className="text-green-800">
+                              Correct Answer:{" "}
+                              {quiz.choices[quiz.correctAnswerIndex]}
+                            </span>
+                          </FieldContent>
+                        </Field>
+                      </FieldLabel>
+                    </>
+                  )}
+                </CardContent>
+              </>
+            )}
+            {!answered && (
+              <>
+                <CardHeader>
+                  <CardTitle>
+                    {currentQuizIndex + 1}. {quiz.question}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <RadioGroup
+                    onValueChange={setSelectedValue}
+                    value={selectedValue}
+                  >
+                    {quiz.choices.map((choice, index) => (
+                      <FieldLabel
+                        key={index}
+                        className={
+                          correctAnswered &&
+                          index.toString() == quiz.correctAnswerIndex.toString()
+                            ? "bg-green-400"
+                            : ""
+                        }
+                      >
+                        <Field orientation="horizontal">
+                          <FieldContent>{choice}</FieldContent>
+                          <RadioGroupItem value={index.toString()} />
+                        </Field>
+                      </FieldLabel>
+                    ))}
+                  </RadioGroup>
+                </CardContent>
+              </>
+            )}
+          </Card>
+          <Button
+            className="w-full max-w-sm"
+            onClick={handleClick}
+            disabled={currentQuizIndex > quizs.length - 1}
+          >
+            Choose
+          </Button>
+        </>
+      )}
     </>
   );
 };
